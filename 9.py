4b0522964ca60092974311da19b165b5a17a3f81
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-03 06:12:52 +0530

    Delete 9.py

--- 9.py
+++
@@ -1,5 +0,0 @@
-ch=(input("enter the character:"))
-if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
-    print("IT IS A VOWEL")
-else:
-    print("IT IS CONSONENT")
