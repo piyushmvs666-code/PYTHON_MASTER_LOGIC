5a23bad1f2fd1b7e258976b19b580d47b94d95df
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-05 23:30:04 +0530

    Create EXP_15.py

---
+++ EXP_15.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+if n % 7 == 0 or n % 10 == 7:
+    print("Number is a multiple of 7 or ends with 7.")
+else:
+    print("Number is neither a multiple of 7 nor ends with 7.")
