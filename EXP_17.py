2f1c6a3daf0350cce8772c6c79d2af2f16d91147
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-05 23:32:55 +0530

    Create EXP_17.py

---
+++ EXP_17.py
@@ -0,0 +1,6 @@
+amount = int(input("Enter the amount: "))
+
+if amount % 100 == 0:
+    print("Amount can be divided into 2000, 500, and 100 notes.")
+else:
+    print("Amount cannot be evenly divided into these notes.")
