2e834362eddd4b9a999cce237fe69c8ac660d4b9
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-20 21:08:20 +0530

    Create EXP_57.py

---
+++ EXP_57.py
@@ -0,0 +1,13 @@
+# 2. Check if a number is a perfect number
+
+num = int(input("Enter a number: "))
+sum_of_divisors = 0
+
+for i in range(1, num):
+    if num % i == 0:
+        sum_of_divisors += i
+
+if sum_of_divisors == num:
+    print(num, "is a perfect number")
+else:
+    print(num, "is not a perfect number")
