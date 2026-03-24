47f61e9f580f142355e0b2dd05f7595cb3bd1057
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-24 23:27:08 +0530

    Create EXP_67.py

---
+++ EXP_67.py
@@ -0,0 +1,9 @@
+# 6. Print all factors of a given number.
+
+num = int(input("Enter a number: "))
+
+print("Factors of", num, "are:")
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i)
