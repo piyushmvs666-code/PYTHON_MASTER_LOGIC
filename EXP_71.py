696a24ca8d9dda5f787a1c2e142bab0b667019e1
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-26 23:03:56 +0530

    Create EXP_71.py

---
+++ EXP_71.py
@@ -0,0 +1,9 @@
+# 2. Take 5 numbers as input. If the user enters 0, skip it using continue.
+#    At the end, print the sum of all non-zero numbers entered.
+total = 0
+for i in range(5):
+    num = int(input(f"Enter number {i+1}: "))
+        if num == 0:
+        continue
+        total += num
+print("Sum of non-zero numbers:", total)
