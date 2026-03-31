4b16069e1d8198cf0c7e6769eb5e05c666d34dd0
Piyush Kumar <piyushmvs666@gmail.com>
2026-03-31 23:27:14 +0530

    Create EXP_80.py

---
+++ EXP_80.py
@@ -0,0 +1,7 @@
+# 1. Print all numbers that are palindromes between 1–500
+
+print("Palindromes between 1 and 500:")
+for num in range(1, 501):
+    if str(num) == str(num)[::-1]:
+        print(num, end=" ")
+print("\n")
