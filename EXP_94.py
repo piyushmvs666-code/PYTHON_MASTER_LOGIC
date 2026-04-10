2bfcd4d397acf29ea1d01090dfec0f5f44e7415c
Piyush Kumar <piyushmvs666@gmail.com>
2026-04-10 21:01:18 +0530

    Create EXP_94.py

---
+++ EXP_94.py
@@ -0,0 +1,9 @@
+# 2. Find the sum of all factors of a number
+num = int(input("Enter a number: "))
+
+sum_of_factors = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        sum_of_factors += i
+
+print("Sum of all factors of", num, "is:", sum_of_factors)
