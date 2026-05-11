2b9f5b3e92cf5ac5648ff7d9691afd20dea060cb
Piyush Kumar <piyushmvs666@gmail.com>
2026-05-11 23:46:46 +0530

    Create EXP_153.py

---
+++ EXP_153.py
@@ -0,0 +1,15 @@
+# Take a character and check whether it’s uppercase, lowercase, a digit, or a special
+#character.
+ch = input("Enter a character: ")
+
+if len(ch) != 1:
+    print("Please enter only one character.")
+else:
+    if ch >= 'A' and ch <= 'Z':
+        print("Uppercase Letter")
+    elif ch >= 'a' and ch <= 'z':
+        print("Lowercase Letter")
+    elif ch >= '0' and ch <= '9':
+        print("Digit")
+    else:
+        print("Special Character")
