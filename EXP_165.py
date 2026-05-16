2ded131bde47f7c03b7274c9b17164f557dde968
Piyush Kumar <piyushmvs666@gmail.com>
2026-05-16 23:25:28 +0530

    Create EXP_165.py

---
+++ EXP_165.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+if 100 <= n <= 999:
+    print("Number lies within the range [100, 999].")
+else:
+    print("Number does not lie within the range [100, 999].")
