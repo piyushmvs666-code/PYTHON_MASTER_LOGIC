365d2e0c9fc9c78754ff08b049107deeff0c4ee8
Piyush Kumar <piyushmvs666@gmail.com>
2026-06-23 22:23:26 +0530

    Add prime number checker in EXP_186.py
    Implement a program to check for prime numbers.

---
+++ EXP_186.py
@@ -0,0 +1,12 @@
+# Write a Python program to check whether a given number is a Prime Number or not.
+  num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a Prime Number")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a Prime Number")
+            break
+    else:
+        print("Prime Number")
