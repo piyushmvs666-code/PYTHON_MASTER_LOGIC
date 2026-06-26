f7c9cbd3a7415723e01f43390f0a4977d134dc30
Piyush Kumar <piyushmvs666@gmail.com>
2026-06-26 20:47:44 +0530

    Add Armstrong Number checker in EXP_188.py
    Implement a program to check for Armstrong Numbers.

---
+++ EXP_188.py
@@ -0,0 +1,16 @@
+# Write a Python program to check whether a given number is an Armstrong Number or not.
+
+num = int(input("Enter a number: "))
+temp = num
+digits = len(str(num))
+sum_val = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_val += digit ** digits
+    temp //= 10
+
+if sum_val == num:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
