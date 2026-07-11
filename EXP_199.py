e618eb38dd64cfebfec4a240722f05bd93505ce8
Piyush Kumar <piyushmvs666@gmail.com>
2026-07-11 20:28:30 +0530

    Create EXP_199.py

---
+++ EXP_199.py
@@ -0,0 +1,9 @@
+# Find Two Numbers That Add Up to a Target.
+  def find_two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+      
