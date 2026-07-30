ef9e1aeef6e9e911da97df7633ce85de008a0429
Piyush Kumar <piyushmvs666@gmail.com>
2026-07-30 23:29:10 +0530

    Update EXP_1.py

--- EXP_1.py
+++ EXP_1.py
@@ -1,4 +1,4 @@
-#Take a number and print whether it’s positive, negative, or zero.
+# Q.Take a number and print whether it’s positive, negative, or zero.
 num = float(input("Enter a number: "))
 
 if num > 0:
