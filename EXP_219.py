62de8afbf442c063be2b477d6cd1860df3261355
Piyush Kumar <piyushmvs666@gmail.com>
2026-08-08 17:34:44 +0530

    Create EXP_219.py

---
+++ EXP_219.py
@@ -0,0 +1,11 @@
+# Write a program that prints numbers from 1 to 15. For multiples of 3, print "Fizz" instead of the number. For multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+  def fizzbuzz(limit):
+    for i in range(1, limit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
