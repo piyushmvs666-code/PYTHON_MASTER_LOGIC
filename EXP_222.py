1317afc97808471e1f01e12ad310b7b31c1a7fa5
Piyush Kumar <piyushmvs666@gmail.com>
2026-08-11 23:10:48 +0530

    Create EXP_222.py

---
+++ EXP_222.py
@@ -0,0 +1,8 @@
+# Write a Python function that takes a string as input and returns it reversed.
+  def reverse_string(text):
+    return text[::-1]
+
+# Test the function
+print(reverse_string("Python"))
+  
+  
