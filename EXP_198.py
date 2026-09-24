f8ab0486e49af1d52d30b843e8cc99bee79e38a2
Piyush Kumar <piyushmvs666@gmail.com>
2026-09-24 23:55:36 +0530

    Delete EXP_198.py

--- EXP_198.py
+++
@@ -1,7 +0,0 @@
-# Find Two Numbers That Add Up to a Target.
-  def two_sum(nums: list[int], target: int) -> list[int]
-    for current_index, num in enumerate(nums):
-        complement = target - num
-        if complement in seen_numbers:
-            return [seen_numbers[complement], current_index]     
-    return []  
