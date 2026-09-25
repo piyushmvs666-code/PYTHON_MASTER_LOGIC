4ae12b38fe582d09d85e603b0371d6848873993d
Piyush Kumar <piyushmvs666@gmail.com>
2026-09-25 21:42:10 +0530

    Delete EXP_199.py

--- EXP_199.py
+++
@@ -1,9 +0,0 @@
-# Find Two Numbers That Add Up to a Target.
-  def find_two_sum(nums, target):
-    seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
-      
